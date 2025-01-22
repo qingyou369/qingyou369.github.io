9cf2ba41388ab04e1e7e1185da1e500e9ce4f73f
qingyou369 <3364248817@qq.com>
2025-01-22 09:16:03 +0800

    Site updated: 2025-01-22 09:16:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/19/1/","2025/01/14/hello-world/","2025/01/19/2/"];function toRandomPost(){
+var posts=["2025/01/19/1/","2025/01/19/2/","2025/01/14/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
